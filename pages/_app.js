911a68c4801d930ad90140eb029d6ea76799b127
Aymane D. Hlamnach <dara-@outlook.jp>
2021-07-21 23:18:14 +0100

    no-under false

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import Layout from 'components/shared/Layout'
-import 'assets/scss/main.scss'
-
+import Layout from 'components/shared/Layout';
+import 'assets/scss/main.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
