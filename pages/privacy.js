1e69db5fcd5158258dfd62ddac540364c6675d93
Aymane D. Hlamnach <dara-@outlook.jp>
2021-07-22 08:31:07 +0100

    policy & terms filler

---
+++ pages/privacy.js
@@ -0,0 +1,9 @@
+export default function Privacy() {
+  return (
+    <div className="contentContainer">
+      <h1 style={{ fontSize: '55px', textAlign: 'center' }}>
+        Under Construction
+      </h1>
+    </div>
+  );
+}
