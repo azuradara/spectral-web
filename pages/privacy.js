8e46a4bf730b79f6b1c950870944bed4064566f0
Aymane D. Hlamnach <dara-@outlook.jp>
2021-07-22 10:05:52 +0100

    privacy policy extension compliance

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,13 +1,62 @@
 import Head from 'next/head';
-export default function Privacy() {
+import Link from 'next/link';
+
+import fs from 'fs';
+import path from 'path';
+import * as matter from 'gray-matter';
+import marked from 'marked';
+
+export default function Privacy({ log }) {
   return (
-    <div className="contentContainer">
+    <div className="contentContainer page privacy">
       <Head>
         <title>Spectral - Privacy Policy</title>
       </Head>
-      <h1 style={{ fontSize: '55px', textAlign: 'center' }}>
-        Under Construction
-      </h1>
+
+      <div className="heading">
+        <div className="left"></div>
+        <div className="hero">
+          <Link href="/changelog">
+            <h1>Privacy Policy</h1>
+          </Link>
+        </div>
+      </div>
+
+      <div className="posts">
+        <div key={log.slug}>
+          <hr />
+          <div className="post">
+            <div className="right">
+              <div>
+                {log.data.cover_image && (
+                  <figure>
+                    <img src={log.data.cover_image} />
+                  </figure>
+                )}
+                <div
+                  dangerouslySetInnerHTML={{ __html: marked(log.content) }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const file = fs.readFileSync(path.join('markdown/privacy.md'));
+  const { data, content } = matter(file);
+
+  const slug = data.title
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-');
+
+  return {
+    props: {
+      log: { data, content, slug },
+    },
+  };
 }
