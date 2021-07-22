660c38d03314c23573c92b7d28d9b2d7ed79090e
Aymane D. Hlamnach <dara-@outlook.jp>
2021-07-22 02:45:27 +0100

    google verification

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import Layout from 'components/shared/Layout';
 import 'assets/scss/main.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="xfu43kwf7sgbE96KISAuyPxuXMe1Rd_Tl703JL81b00"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
